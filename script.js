551c4c58474a56848e9a39fe17a6a1b3a60de24e
rajkhare05 <06rajkhare04@gmail.com>
2021-08-25 21:42:58 +0530

    clsb3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let box1 = document.getElementById("inputBox");
 let add_btn = document.getElementById("add-btn");
-let task_list = document.getElementById("list");
+let container = document.getElementById("container");
+let len_para = document.getElementById("text-len");
 let unique_id = 1;
 let tasks = [];
 
@@ -8,28 +9,48 @@ let tasks = [];
 const addTask = () => {
     if (box1.value != ""){
 
-        // class name for checbox and task (text)
+        if (box1.value.length > 100) {
+            alert("100 characters are allowed !");
+            return;
+        }
+
+        // class name for main task box
         let cls_name = "task" + unique_id;
+        let main_box = document.createElement("div");
+        main_box.setAttribute("class", cls_name);
+
+        // class name for checkbox and task (text)
+        let sub_cls_name = "t" + unique_id;
 
         // checkbox for marking task complete / incomplete
         let ch_box = document.createElement("input");
         ch_box.setAttribute("type", "checkbox");
-        ch_box.setAttribute("class", cls_name);
-        ch_box.setAttribute("onclick", "checkTask(this)");
+        ch_box.setAttribute("class", sub_cls_name);
+        ch_box.setAttribute("onclick", "markTask(this)");
         
         // labeling task name and other details
         let new_task = document.createElement("label");
-        new_task.setAttribute("class", cls_name);
-        new_task.setAttribute("onclick", "checkTask(this)");
-        new_task.innerText = box1.value;
+        new_task.setAttribute("class", sub_cls_name);
+        new_task.setAttribute("onclick", "markTask(this)");
+        new_task.textContent = box1.value;
+
+        // remove task button
+        let rem_btn = document.createElement("button");
+        rem_btn.setAttribute("class", sub_cls_name);
+        rem_btn.setAttribute("onclick", "removeTask(this)");
+        rem_btn.textContent = "remove";
         
         // new line after each task
         let break_el = document.createElement("br");
         
         // appending elements into main container (= 'list' div tag)
-        task_list.appendChild(ch_box);
-        task_list.appendChild(new_task);
-        task_list.appendChild(break_el);
+        main_box.appendChild(ch_box);
+        main_box.appendChild(new_task);
+        main_box.appendChild(rem_btn);
+        main_box.appendChild(break_el);
+
+        // appending task div inside container
+        container.appendChild(main_box);
         
         // appending task into array of tasks
         tasks.push(box1.value);
@@ -47,13 +68,22 @@ box1.addEventListener('keyup', (event) => {
     }
 });
 
+box1.addEventListener('input', () => {
+    let text_len = box1.value.length;
+    len_para.textContent = text_len;
+    if (text_len > 100){
+        alert("100 characters are allowed !");
+        return;
+    }
+});
+
 // add task on clicking 'ADD' button
 add_btn.addEventListener('click', () => {
     addTask();
 });
 
 // mark task complete / incomplete by line-through property
-const checkTask = (element) => {
+const markTask = (element) => {
 
     // if checkbox is checked / unchecked
     if (element.tagName == "INPUT"){
@@ -61,7 +91,7 @@ const checkTask = (element) => {
         let text_box = document.getElementsByClassName(element.className)[1];
 
         if (element.checked == true){
-            text_box.style.textDecoration = "line-through";
+            text_box.style.textDecoration = "line-through double red";
         } else {
             text_box.style.textDecoration = "none";
         }
@@ -74,8 +104,15 @@ const checkTask = (element) => {
             element.style.textDecoration = "none";
             check_box.checked = false;
         } else {
-            element.style.textDecoration = "line-through";
+            element.style.textDecoration = "line-through double red";
             check_box.checked = true;
         }
     }
+}
+
+// remove task
+const removeTask = (element) => {
+    let all_elements = document.getElementsByClassName(element.parentNode.className);
+    all_elements[0].remove();
+    element.remove();
 }
